b4bfc31412d6eaffecba5f7265ef359296815a29
Denis <denismak123@gmail.com>
2021-08-10 00:51:59 +0300

    Date fix

--- validation.py
+++ validation.py
@@ -1,15 +1,17 @@
 import abc
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.multioutput import MultiOutputRegressor
 from matplotlib import pyplot as plt
-from itertools import product
 from matplotlib.colors import Normalize
+from itertools import product
 
 class TimeSeriesCV(abc.ABC):
-    def __init__(self, estimator, test_size, fig, axs, param_grid):
+    def __init__(self, estimator, test_size, fig, axs, dates, param_grid):
         self.estimator_cls = estimator
         self.param_grid = param_grid
         self.test_size = test_size
+        self.dates = dates.copy()
 
         self.estimator = None
         self.best_errors_sequence = [[None] * 3 for _ in range(test_size)]
@@ -30,7 +32,7 @@ class TimeSeriesCV(abc.ABC):
         colors = plt.cm.plasma(np.linspace(0, 1, self.test_size))
         for h_i in range(self.test_size):
             for ax, error in zip(self.axs, errors[h_i]):
-                ax.plot(range(n), error, color=colors[h_i])
+                ax.plot(self.dates, error, color=colors[h_i])
         self.fig.colorbar(plt.cm.ScalarMappable(norm=Normalize(vmin=1, vmax=self.test_size), cmap='plasma'), ax=self.axs)
 
     def create_param_grid(self):
@@ -41,9 +43,9 @@ class TimeSeriesCV(abc.ABC):
 
     def test(self, X_train, Y_train, params):
         if params:
-            self.estimator = self.estimator_cls(**params)
+            self.estimator = MultiOutputRegressor(self.estimator_cls(**params))
         else:
-            self.estimator = self.estimator_cls()
+            self.estimator = MultiOutputRegressor(self.estimator_cls())
 
         self.estimator.fit(X_train, Y_train)
 
@@ -68,15 +70,17 @@ class TimeSeriesCV(abc.ABC):
 
 class TimeSeriesWindowCV(TimeSeriesCV):
 
-    def __init__(self, estimator, train_size, test_size, shift, fig, axs, param_grid=None):
-        super().__init__(estimator, test_size, fig, axs, param_grid)
+    def __init__(self, estimator, train_size, test_size, shift, fig, axs, dates, param_grid=None):
+        super().__init__(estimator, test_size, fig, axs, dates, param_grid)
 
         self.train_size = train_size
         self.shift = shift
+        self.dates = self.dates[self.train_size:-2*self.test_size:self.shift]
 
     def make_forecasts(self, X, Y, metric, params):
         window_size = self.train_size + self.test_size
-        errors, i = np.zeros(shape=(self.test_size, (len(X) - window_size - self.test_size) // self.shift + 1, 3)), 0
+        n = (len(X) - window_size - self.test_size) // self.shift + 1
+        errors, i = np.zeros(shape=(self.test_size, n, 3)), 0
 
         for i in range(self.test_size):
             best_errors_mean = [np.inf] * 3
@@ -93,18 +97,19 @@ class TimeSeriesWindowCV(TimeSeriesCV):
 
                 start += self.shift
                 j += 1
-                
+            print(f'{(i + 1) / (self.test_size) * 100:.2f}%', end='\r')    
             self.update_errors(i, errors, best_errors_mean, params)
 
 
 class TimeSeriesWalkingForwardCV(TimeSeriesCV):
-    def __init__(self, estimator, test_size, n_splits, fig, axs, gap=0, param_grid=None):
-        super().__init__(estimator, test_size, fig, axs, param_grid)
+    def __init__(self, estimator, test_size, n_splits, fig, axs, dates, gap=0, param_grid=None):
+        super().__init__(estimator, test_size, fig, axs, dates, param_grid)
 
         self.test_size = test_size
         self.n_splits = n_splits
         self.gap = gap
         self.ts_split = TimeSeriesSplit(test_size=self.test_size, n_splits=self.n_splits, gap=self.gap)
+        self.dates = self.dates.iloc[[a[-1] for a, b in list(self.ts_split.split(self.dates))]]
 
     def make_forecasts(self, X, Y, metric, params):
         errors = np.zeros(shape=(self.test_size, self.n_splits, 3))
@@ -121,5 +126,5 @@ class TimeSeriesWalkingForwardCV(TimeSeriesCV):
 
                 errors[i][j] = metric(Y_pred, Y_test)
                 j += 1
-
+            print(f'{(i + 1) / (self.test_size) * 100:.2f}%', end='\r')    
             self.update_errors(i, errors, best_errors_mean, params)
